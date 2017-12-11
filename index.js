4b9b3e9377b9999c09d4c5e6b952086b0f9be054
Krzysztof Rutkowski <krzysztof.rutkowski.86@gmail.com>
2017-12-11 10:17:04 +0100

    parse error handling

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ module.exports = function (fileName, joinCwd) {
 
   var data = {};
   var firstFile = null;
+  var error = false;
 
   function bufferContents(file) {
     if (!firstFile) {
@@ -24,21 +25,29 @@ module.exports = function (fileName, joinCwd) {
     if (file.isStream()) {
       return this.emit('error', new PluginError('gulp-json-concat-with-path', 'Streaming not supported.'));
     }
-    
-    var key = joinCwd ? path.relative(path.join(file.cwd,joinCwd),file.path) : path.relative(file.cwd,file.path);
-    data[key] = JSON.parse(file.contents.toString());
+
+    try {
+      var key = joinCwd ? path.relative(path.join(file.cwd,joinCwd),file.path) : path.relative(file.cwd,file.path);
+      data[key] = JSON.parse(file.contents.toString());
+    } catch (err) {
+      error = true;
+      return this.emit('error', new PluginError('gulp-json-concat-with-path', 'Error parsing JSON: ' + err + ', file: ' + file.path));
+    }
+
   }
 
   function endStream() {
-    var filePath = path.join(firstFile.base, fileName);
-    var file = new File({
-      cwd: firstFile.cwd,
-      base: firstFile.base,
-      path: filePath,
-      contents: new Buffer(JSON.stringify(data))
-    });
+    if(!error){
+      var filePath = path.join(firstFile.base, fileName);
+      var file = new File({
+        cwd: firstFile.cwd,
+        base: firstFile.base,
+        path: filePath,
+        contents: new Buffer(JSON.stringify(data))
+      });
 
-    this.emit('data', file);
+      this.emit('data', file);
+    }
     this.emit('end');
   }
 
